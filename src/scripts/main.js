a37a9c0edfe9a0db9551a25121efec7d4094ab6b
Nazariy <nazariypanochko@gmail.com>
2025-08-05 18:08:19 +0300

    SOL

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,40 +2,36 @@
 
 const pushNotification = (posTop, posRight, title, description, type) => {
   const notification = document.createElement('div');
-  const body = document.body;
 
   notification.classList.add('notification', type);
-
   notification.style.position = 'fixed';
+  notification.style.top = posTop + 'px';
+  notification.style.right = posRight + 'px';
 
   const h2 = document.createElement('h2');
 
   h2.classList.add('title');
-
   h2.textContent = title;
-  notification.append(h2);
 
   const p = document.createElement('p');
 
+  p.classList.add('description');
   p.textContent = description;
 
-  notification.append(p);
-
-  notification.style.top = posTop + 'px';
-  notification.style.right = posRight + 'px';
-
-  body.append(notification);
+  notification.append(h2, p);
+  document.body.append(notification);
 
   setTimeout(() => {
-    notification.style.display = 'none';
+    notification.remove();
   }, 2000);
 };
 
+// Виклики
 pushNotification(
   10,
   10,
   'Title of Success message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example. Notification should contain title and description.',
   'success',
 );
 
@@ -43,7 +39,7 @@ pushNotification(
   163,
   10,
   'Title of Error message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example. Notification should contain title and description.',
   'error',
 );
 
@@ -51,6 +47,6 @@ pushNotification(
   287,
   10,
   'Title of Warning message',
-  'Message example.\n ' + 'Notification should contain title and description.',
+  'Message example. Notification should contain title and description.',
   'warning',
 );
